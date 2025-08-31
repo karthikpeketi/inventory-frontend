5933fee11bd51164d3fa10a5e716e17cf32aeae1
karthikpeketi <karthikreddypeketi@gmail.com>
2025-09-01 00:35:21 +0530

    style: Adjust indentation in Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: './', // important for Vercel
+  base: './', // important for Vercel
   plugins: [
     react(), 
     VitePWA({
